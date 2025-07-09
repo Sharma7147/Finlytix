d7d522b257308ebb011568f9f3f37e9f2cdb8371
Sharma7147 <yashrajsharma1910@gmail.com>
2025-07-09 18:29:28 +0530

    update

--- server/index.js
+++ server/index.js
@@ -8,7 +8,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV === 'development') {
+
+if (process.env.NODE_ENV !== 'production') {
   validateRoutes(app);
 }
 // ✅ Allowed frontend origins (Dev & Production)
