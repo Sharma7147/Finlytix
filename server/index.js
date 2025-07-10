714adf724b1ddfb2d736ac037f13fc2244cb1b28
Sharma7147 <118017416+Sharma7147@users.noreply.github.com>
2025-07-10 21:34:30 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const validateRoutes = require('./middleware/routeValidator'); // Add this line
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT =  5000;
 
 
 if (process.env.NODE_ENV !== 'production') {
